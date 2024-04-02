589ee9115c847c46159a933b4946b96ee5cca688
Arthur Guest <arthur@clara.co>
2024-04-02 20:56:34 +0530

    Add incoming issuance validity and incoming date validity to stock transactions

--- src/validators/stock/tx_stock_reissuance.ts
+++ src/validators/stock/tx_stock_reissuance.ts
@@ -1,7 +1,53 @@
+import {transactions} from '../../test_data/data';
+
 const valid_tx_stock_reissuance = (context: any, event: any) => {
   let valid = false;
   valid = true;
   // TBC: validation of tx_stock_reissuance
+
+  // Check that stock issuance in incoming security_id referenced by transaction exists in current state.
+  let incoming_stockIssuance_validity = false;
+  context.stockIssuances.forEach((ele: any) => {
+    if (
+      ele.security_id === event.data.security_id &&
+      ele.object_type === 'TX_STOCK_ISSUANCE'
+    ) {
+      incoming_stockIssuance_validity = true;
+      console.log(
+        `\x1b[92m\u2714 The incoming security (${event.data.security_id}) for this reissuance exists.\x1b[0m`
+      );
+    }
+  });
+  if (!incoming_stockIssuance_validity) {
+    console.log(
+      `\x1b[91m\u2718 The incoming security (${event.data.security_id}) for this reissuance does not exist in the current cap table.\x1b[0m`
+    );
+  }
+
+  // Check to ensure that the date of transaction is the same day or after the date of the incoming stock issuance.
+  let incoming_date_validity = false;
+  transactions.items.forEach((ele: any) => {
+    if (
+      ele.security_id === event.data.security_id &&
+      ele.object_type === 'TX_STOCK_ISSUANCE'
+    ) {
+      if (ele.date <= event.data.date) {
+        incoming_date_validity = true;
+        console.log(
+          `\x1b[92m\u2714 The date of this reissuance is on or after the date of the incoming security (${event.data.security_id}).\x1b[0m`
+        );
+      }
+    }
+  });
+  if (!incoming_date_validity) {
+    console.log(
+      `\x1b[91m\u2718 The date of this reissuance is not on or after the date of the incoming security (${event.data.security_id}).\x1b[0m`
+    );
+  }
+
+  if (incoming_stockIssuance_validity && incoming_date_validity) {
+    valid = true;
+  }
 
   if (valid) {
     return true;
