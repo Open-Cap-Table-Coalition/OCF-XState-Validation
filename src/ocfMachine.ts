f765ef46c54f6629a0f50406846fff1d47d400df
Arthur Guest <arthur@clara.co>
2024-06-13 15:48:22 +0530

    Added all transactions to the engine
    Including convertible, warrant, equity compensation transactions to the xstate machine

--- src/ocfMachine.ts
+++ src/ocfMachine.ts
@@ -3,6 +3,9 @@ import validators from './validators';
 
 export type OcfMachineContext = {
   stockIssuances: any[];
+  optionGrants: any[];
+  convertibleIssuances: any[];
+  warrantIssuances: any[];
   ocfPackageContent: OcfPackageContent;
 };
 
@@ -10,7 +13,13 @@ const ocfMachine: any = {
   predictableActionArguments: true,
   id: 'OCF-xstate',
   initial: 'capTable',
-  context: {stockIssuances: [], ocfPackageContent: {}},
+  context: {
+    stockIssuances: [],
+    optionGrants: [],
+    convertibleIssuances: [],
+    warrantIssuances: [],
+    ocfPackageContent: {},
+  },
   states: {
     capTable: {
       on: {
@@ -20,38 +29,6 @@ const ocfMachine: any = {
             cond: validators.valid_tx_stock_issuance,
             actions: (context: any, event: any) => {
               context.stockIssuances.push(event.data);
-            },
-          },
-          {
-            target: 'validationError',
-          },
-        ],
-        TX_STOCK_TRANSFER: [
-          {
-            target: 'capTable',
-            cond: validators.valid_tx_stock_transfer,
-            actions: (context: any, event: any) => {
-              context.stockIssuances = context.stockIssuances.filter(
-                (obj: any) => {
-                  return obj.security_id !== event.data.security_id;
-                }
-              );
-            },
-          },
-          {
-            target: 'validationError',
-          },
-        ],
-        TX_STOCK_CANCELLATION: [
-          {
-            target: 'capTable',
-            cond: validators.valid_tx_stock_cancellation,
-            actions: (context: any, event: any) => {
-              context.stockIssuances = context.stockIssuances.filter(
-                (obj: any) => {
-                  return obj.security_id !== event.data.security_id;
-                }
-              );
             },
           },
           {
@@ -78,22 +55,15 @@ const ocfMachine: any = {
           {
             target: 'capTable',
             cond: validators.valid_tx_stock_acceptance,
-            actions: (context: any, event: any) => {
-              // context.stockIssuances = context.stockIssuances.filter(
-              //   (obj: any) => {
-              //     return obj.security_id !== event.data.security_id;
-              //   }
-              // );
-            },
           },
           {
             target: 'validationError',
           },
         ],
-        TX_STOCK_REISSUANCE: [
+        TX_STOCK_CANCELLATION: [
           {
             target: 'capTable',
-            cond: validators.valid_tx_stock_reissuance,
+            cond: validators.valid_tx_stock_cancellation,
             actions: (context: any, event: any) => {
               context.stockIssuances = context.stockIssuances.filter(
                 (obj: any) => {
@@ -122,6 +92,22 @@ const ocfMachine: any = {
             target: 'validationError',
           },
         ],
+        TX_STOCK_REISSUANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_stock_reissuance,
+            actions: (context: any, event: any) => {
+              context.stockIssuances = context.stockIssuances.filter(
+                (obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                }
+              );
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
         TX_STOCK_REPURCHASE: [
           {
             target: 'capTable',
@@ -138,23 +124,476 @@ const ocfMachine: any = {
             target: 'validationError',
           },
         ],
+        TX_STOCK_TRANSFER: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_stock_transfer,
+            actions: (context: any, event: any) => {
+              context.stockIssuances = context.stockIssuances.filter(
+                (obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                }
+              );
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_CONVERTIBLE_ISSUANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_convertible_issuance,
+            actions: (context: any, event: any) => {
+              context.convertibleIssuances.push(event.data);
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_CONVERTIBLE_RETRACTION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_convertible_retraction,
+            actions: (context: any, event: any) => {
+              context.convertibleIssuances =
+                context.convertibleIssuances.filter((obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_CONVERTIBLE_ACCEPTANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_convertible_acceptance,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_CONVERTIBLE_CANCELLATION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_convertible_cancellation,
+            actions: (context: any, event: any) => {
+              context.convertibleIssuances =
+                context.convertibleIssuances.filter((obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_CONVERTIBLE_TRANSFER: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_convertible_transfer,
+            actions: (context: any, event: any) => {
+              context.convertibleIssuances =
+                context.convertibleIssuances.filter((obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_CONVERTIBLE_CONVERSION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_convertible_conversion,
+            actions: (context: any, event: any) => {
+              context.convertibleIssuances =
+                context.convertibleIssuances.filter((obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_WARRANT_ISSUANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_warrant_issuance,
+            actions: (context: any, event: any) => {
+              context.warrantIssuances.push(event.data);
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_WARRANT_RETRACTION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_warrant_retraction,
+            actions: (context: any, event: any) => {
+              context.warrantIssuances = context.warrantIssuances.filter(
+                (obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                }
+              );
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_WARRANT_ACCEPTANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_warrant_acceptance,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_WARRANT_CANCELLATION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_warrant_cancellation,
+            actions: (context: any, event: any) => {
+              context.warrantIssuances = context.warrantIssuances.filter(
+                (obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                }
+              );
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_WARRANT_TRANSFER: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_warrant_transfer,
+            actions: (context: any, event: any) => {
+              context.warrantIssuances = context.warrantIssuances.filter(
+                (obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                }
+              );
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_WARRANT_EXERCISE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_warrant_exercise,
+            actions: (context: any, event: any) => {
+              context.warrantIssuances = context.warrantIssuances.filter(
+                (obj: any) => {
+                  return obj.security_id !== event.data.security_id;
+                }
+              );
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_ISSUANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_issuance,
+            actions: (context: any, event: any) => {
+              context.optionGrants.push(event.data);
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_RETRACTION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_retraction,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_ACCEPTANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_acceptance,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_CANCELLATION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_cancellation,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_TRANSFER: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_transfer,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_RELEASE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_release,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_EQUITY_COMPENSATION_EXERCISE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_equity_compensation_exercise,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_ISSUANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_issuance,
+            actions: (context: any, event: any) => {
+              context.optionGrants.push(event.data);
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_RETRACTION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_retraction,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_ACCEPTANCE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_acceptance,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_CANCELLATION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_cancellation,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_TRANSFER: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_transfer,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_RELEASE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_release,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_PLAN_SECURITY_EXERCISE: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_plan_security_exercise,
+            actions: (context: any, event: any) => {
+              context.optionGrants = context.optionGrants.filter((obj: any) => {
+                return obj.security_id !== event.data.security_id;
+              });
+            },
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_STOCK_CLASS_AUTHORIZED_SHARES_ADJUSTMENT: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_stock_class_authorized_shares_adjustment,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_STOCK_CLASS_CONVERSION_RATIO_ADJUSTMENT: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_stock_class_conversion_ratio_adjustment,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_STOCK_CLASS_SPLIT: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_stock_class_split,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_VESTING_ACCELERATION: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_vesting_acceleration,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_VESTING_EVENT: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_vesting_event,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
+        TX_VESTING_START: [
+          {
+            target: 'capTable',
+            cond: validators.valid_tx_vesting_start,
+          },
+          {
+            target: 'validationError',
+          },
+        ],
         RUN_EOD: {
           taget: 'capTable',
           actions: (context: any, event: any) => {
             console.log(
               `\x1b[0m\nThe cap table as of the end of day on ${event.date}:\x1b[0m`
             );
-            const table: any[] = [];
+            const stock_table: any[] = [];
             context.stockIssuances.forEach((issuance: any) => {
-              table.push({
+              stock_table.push({
                 'Equity type': 'Stock',
+                'Issue date': issuance.date,
                 'Custom ID': issuance.custom_id,
-                'Equity holder': issuance.stakeholder_id,
+                Stockholder: issuance.stakeholder_id,
                 'Stock class': issuance.stock_class_id,
                 Quantity: issuance.quantity,
               });
             });
-            console.table(table);
+            if (stock_table.length > 0) {
+              console.table(stock_table);
+            } else {
+              console.log('No outstanding stock positions.');
+            }
+            const convertible_table: any[] = [];
+            context.convertibleIssuances.forEach((issuance: any) => {
+              convertible_table.push({
+                'Equity type': 'Convertible',
+                'Custom ID': issuance.custom_id,
+                'Convertible holder': issuance.stakeholder_id,
+                'Issue Date': issuance.date,
+                'Investment Amount': `${issuance.investment_amount.amount} ${issuance.investment_amount.currency}`,
+              });
+            });
+            if (convertible_table.length > 0) {
+              console.table(convertible_table);
+            } else {
+              console.log('No outstanding convertibles.');
+            }
+            const warrant_table: any[] = [];
+            context.warrantIssuances.forEach((issuance: any) => {
+              warrant_table.push({
+                'Equity type': 'Warrant',
+                'Custom ID': issuance.custom_id,
+                'Warrant holder': issuance.stakeholder_id,
+                'Issue Date': issuance.date,
+                'Investment Amount': `${issuance.investment_amount.amount} ${issuance.investment_amount.currency}`,
+              });
+            });
+            if (warrant_table.length > 0) {
+              console.table(warrant_table);
+            } else {
+              console.log('No outstanding warrants.');
+            }
           },
         },
       },
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,21 +1,89 @@
+import valid_tx_stock_issuance from './stock/tx_stock_issuance';
+import valid_tx_stock_retraction from './stock/tx_stock_retraction';
 import valid_tx_stock_acceptance from './stock/tx_stock_acceptance';
 import valid_tx_stock_cancellation from './stock/tx_stock_cancellation';
 import valid_tx_stock_conversion from './stock/tx_stock_conversion';
-import valid_tx_stock_issuance from './stock/tx_stock_issuance';
 import valid_tx_stock_reissuance from './stock/tx_stock_reissuance';
 import valid_tx_stock_repurchase from './stock/tx_stock_repurchase';
-import valid_tx_stock_retraction from './stock/tx_stock_retraction';
 import valid_tx_stock_transfer from './stock/tx_stock_transfer';
+import valid_tx_convertible_issuance from './convertible/tx_convertible_issuance';
+import valid_tx_convertible_retraction from './convertible/tx_convertible_retraction';
+import valid_tx_convertible_acceptance from './convertible/tx_convertible_acceptance';
+import valid_tx_convertible_cancellation from './convertible/tx_convertible_cancellation';
+import valid_tx_convertible_transfer from './convertible/tx_convertible_transfer';
+import valid_tx_convertible_conversion from './convertible/tx_convertible_conversion';
+import valid_tx_warrant_issuance from './warrant/tx_warrant_issuance';
+import valid_tx_warrant_retraction from './warrant/tx_warrant_retraction';
+import valid_tx_warrant_acceptance from './warrant/tx_warrant_acceptance';
+import valid_tx_warrant_cancellation from './warrant/tx_warrant_cancellation';
+import valid_tx_warrant_transfer from './warrant/tx_warrant_transfer';
+import valid_tx_warrant_exercise from './warrant/tx_warrant_exercise';
+import valid_tx_equity_compensation_issuance from './equity_compensation/tx_equity_compensation_issuance';
+import valid_tx_equity_compensation_retraction from './equity_compensation/tx_equity_compensation_retraction';
+import valid_tx_equity_compensation_acceptance from './equity_compensation/tx_equity_compensation_acceptance';
+import valid_tx_equity_compensation_cancellation from './equity_compensation/tx_equity_compensation_cancellation';
+import valid_tx_equity_compensation_release from './equity_compensation/tx_equity_compensation_release';
+import valid_tx_equity_compensation_transfer from './equity_compensation/tx_equity_compensation_transfer';
+import valid_tx_equity_compensation_exercise from './equity_compensation/tx_equity_compensation_exercise';
+import valid_tx_plan_security_issuance from './plan_security/tx_plan_security_issuance';
+import valid_tx_plan_security_retraction from './plan_security/tx_plan_security_retraction';
+import valid_tx_plan_security_acceptance from './plan_security/tx_plan_security_acceptance';
+import valid_tx_plan_security_cancellation from './plan_security/tx_plan_security_cancellation';
+import valid_tx_plan_security_release from './plan_security/tx_plan_security_release';
+import valid_tx_plan_security_transfer from './plan_security/tx_plan_security_transfer';
+import valid_tx_plan_security_exercise from './plan_security/tx_plan_security_exercise';
+import valid_tx_stock_class_authorized_shares_adjustment from './stock_class/tx_stock_class_authorized_shares_adjustment';
+import valid_tx_stock_class_conversion_ratio_adjustment from './stock_class/tx_stock_class_conversion_ratio_adjustment';
+import valid_tx_stock_class_split from './stock_class/tx_stock_class_split';
+import valid_tx_stock_plan_pool_adjustment from './stock_plan/tx_stock_plan_pool_adjustment';
+import valid_tx_stock_plan_return_to_pool from './stock_plan/tx_stock_plan_return_to_pool';
+import valid_tx_vesting_acceleration from './vesting/ts_vesting_acceleration';
+import valid_tx_vesting_start from './vesting/ts_vesting_start';
+import valid_tx_vesting_event from './vesting/ts_vesting_event';
 
 const validators = {
+  valid_tx_stock_issuance,
+  valid_tx_stock_retraction,
   valid_tx_stock_acceptance,
   valid_tx_stock_cancellation,
   valid_tx_stock_conversion,
-  valid_tx_stock_issuance,
   valid_tx_stock_reissuance,
   valid_tx_stock_repurchase,
-  valid_tx_stock_retraction,
   valid_tx_stock_transfer,
+  valid_tx_convertible_issuance,
+  valid_tx_convertible_retraction,
+  valid_tx_convertible_acceptance,
+  valid_tx_convertible_cancellation,
+  valid_tx_convertible_transfer,
+  valid_tx_convertible_conversion,
+  valid_tx_warrant_issuance,
+  valid_tx_warrant_retraction,
+  valid_tx_warrant_acceptance,
+  valid_tx_warrant_cancellation,
+  valid_tx_warrant_transfer,
+  valid_tx_warrant_exercise,
+  valid_tx_equity_compensation_issuance,
+  valid_tx_equity_compensation_retraction,
+  valid_tx_equity_compensation_acceptance,
+  valid_tx_equity_compensation_cancellation,
+  valid_tx_equity_compensation_release,
+  valid_tx_equity_compensation_transfer,
+  valid_tx_equity_compensation_exercise,
+  valid_tx_plan_security_issuance,
+  valid_tx_plan_security_retraction,
+  valid_tx_plan_security_acceptance,
+  valid_tx_plan_security_cancellation,
+  valid_tx_plan_security_release,
+  valid_tx_plan_security_transfer,
+  valid_tx_plan_security_exercise,
+  valid_tx_stock_class_authorized_shares_adjustment,
+  valid_tx_stock_class_conversion_ratio_adjustment,
+  valid_tx_stock_class_split,
+  valid_tx_stock_plan_pool_adjustment,
+  valid_tx_stock_plan_return_to_pool,
+  valid_tx_vesting_acceleration,
+  valid_tx_vesting_start,
+  valid_tx_vesting_event,
 };
 
 export default validators;
