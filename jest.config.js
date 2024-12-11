bf1e49d81590a5875c042b27392365d55d697b2f
Matthew Cantor <matthewcantor@gmail.com>
2024-12-11 15:59:11 -0500

    set up

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  preset: `ts-jest/presets/default-esm`,
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^vesting_schedule_generator$": "<rootDir>/vesting_schedule_generator",
   },
 };
