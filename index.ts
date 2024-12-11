7de3a365587bb31fbef01aa0ad253ebb64644c0c
Matthew Cantor <matthewcantor@gmail.com>
2024-12-11 16:15:25 -0500

    index

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import { readOcfPackage } from "./read_ocf_package";
-import { generateSchedule } from "./vesting_schedule_generator";
-import { vestingStatusCheck } from "./vesting_status_check";
-import { isoNsoCalculator } from "./iso_nso_calculator";
+import { generateVestingSchedule } from "vesting_schedule_generator";
+import { ISOCalculator } from "iso_nso_calculator";
 import { ocfValidator } from "./ocf_validator";
 import { ocfSnapshot } from "./ocf_snapshot";
 
-module.exports = { readOcfPackage, generateSchedule, vestingStatusCheck, isoNsoCalculator, ocfValidator, ocfSnapshot };
+module.exports = {
+  readOcfPackage,
+  generateVestingSchedule,
+  ISOCalculator,
+  ocfValidator,
+  ocfSnapshot,
+};
