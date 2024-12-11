26188f71cbbea080d41d2175a3c48d8d78cd727e
Matthew Cantor <matthewcantor@gmail.com>
2024-12-11 16:12:59 -0500

    testing scripts

--- testing_scripts/vesting_status.ts
+++ testing_scripts/vesting_status.ts
@@ -1,5 +1,9 @@
-import { VestingScheduleService } from "../vesting_schedule_generator";
+import { generateVestingSchedule } from "vesting_schedule_generator";
 import { OcfPackageContent, readOcfPackage } from "../read_ocf_package";
+import { getVestingScheduleStatus } from "vesting_schedule_generator/getVestingScheduleStatus";
+import { getOCFDataBySecurityId } from "vesting_schedule_generator/get-ocf-data-by-security-id";
+import { isBefore, parseISO } from "date-fns";
+import { VestingScheduleStatus } from "types";
 
 const packagePath = "./sample_ocf_folders/acme_holdings_limited";
 const securityId = "equity_compensation_issuance_01";
@@ -7,15 +11,36 @@ const ocfPackage: OcfPackageContent = readOcfPackage(packagePath);
 
 try {
   const checkDateString = "2020-06-15";
-  const vestingStatus = new VestingScheduleService(
-    ocfPackage,
-    securityId
-  ).getVestingStatus(checkDateString);
+  const checkDate = parseISO(checkDateString);
+  const schedule = generateVestingSchedule(ocfPackage, securityId);
+  const ocfData = getOCFDataBySecurityId(ocfPackage, securityId);
+  const scheduleWithStatus = getVestingScheduleStatus(schedule, ocfData); // known to be sorted in ascending order
 
-  if (!vestingStatus) {
+  const getlatestInstallment = (
+    schedule: VestingScheduleStatus[],
+    checkDate: Date
+  ) => {
+    let latestInstallment: VestingScheduleStatus | null = null;
+    for (let installment of schedule) {
+      if (isBefore(installment.date, checkDate)) {
+        if (
+          latestInstallment === null ||
+          isBefore(latestInstallment.date, checkDate)
+        ) {
+          latestInstallment = installment;
+        }
+      }
+    }
+    return latestInstallment;
+  };
+
+  const latestInstallment = getlatestInstallment(scheduleWithStatus, checkDate);
+
+  if (latestInstallment === null) {
     console.log("The date provided is before the vesting start date");
+  } else {
+    console.table(latestInstallment);
   }
-  console.table(vestingStatus);
 } catch (error) {
   if (error instanceof Error) {
     console.error("Error message:", error.message);
